f0c2db9d0fd45b0fe568ce0c8f36d34e77b8ea05
pavlos konstantinidis <p.konstantinidis@student.uu.nl>
2021-10-18 17:41:10 +0200

    This simple code created a scatter graph coloring blue the points that are inside the given triangle and red the points outside.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import numpy as nm
+import matplotlib.pyplot as plt
+
+n=500
+x = nm.random.uniform(0.0, 10.0, n)
+y = nm.random.uniform(0.0, 10.0, n)
 
 
+for i in range(0 , n):
+    if (y[i] >= 3) and (x[i] <= 7) and (x[i] >= y[i]):
+        plt.scatter(x[i], y[i], c="blue")
+    else:
+        plt.scatter(x[i], y[i], c="red")
+
+
+plt.show()
